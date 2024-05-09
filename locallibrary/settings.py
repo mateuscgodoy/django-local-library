457ea59807a3bb03675b230a6fe51784ac12f708
Mateus Colombo Godoy <57444793+mateuscgodoy@users.noreply.github.com>
2024-05-09 11:47:34 -0300

    Update settings.py
    Add PythonAnywhere configuration requirements to be able to connect to the website.

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mateuscgodoy.pythonanywhere.com', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ['https://mateuscgodoy.pythonanywhere.com']
 
 # Application definition
 
